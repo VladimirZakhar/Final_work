03b0cbc601f8f59a4adb5424e552f020d20cd258
Vladimir <zvv.ps@ya.ru>
2022-12-24 20:18:24 +0300

    Program writing

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string[] inputArray = {"hello", "2", "world", ":-)" };
+int sizeNewArray = 0;
+int maxLenght = 3;
+Console.Write($"Вводимый массив: ");
+PrintArray(inputArray);
+string[] tempArray = new string[inputArray.Length];
+tempArray = arraySelection(inputArray, maxLenght);
+string[] newArray = new string[sizeNewArray];
+newArray = arrayEditing(tempArray, sizeNewArray);
+Console.Write($"Новый массив: ");
+PrintArray(newArray);
+
+string[] arraySelection(string[] inputArray, int length)
+{
+    string[] tempArray = new string[inputArray.Length];
+    int j = 0;
+    foreach (var item in inputArray)
+    {
+        if (item.Length <= length)
+        {
+            tempArray[j++] = item;
+        }
+    }
+    sizeNewArray = j;
+    return tempArray;
+}
+
+string[] arrayEditing(string[] inputArray, int count)
+{
+    string[] tempArray = new string[count];
+    int i = 0;
+    while (i < count) tempArray[i] = inputArray[i++];
+    return tempArray;
+}
+
+void PrintArray(string[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
